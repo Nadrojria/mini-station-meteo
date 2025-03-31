596ef3da943014bded4aab50bd9b44b8d41b1300
Nadrojria <loicdesbordes@yahoo.fr>
2025-03-31 15:03:10 +0200

    Bonus Chart OK : fin du kata

--- script.js
+++ script.js
@@ -5,8 +5,28 @@ const temperature = document.querySelector('#temperature');
 const details = document.querySelector('#details');
 const precipitation = document.querySelector('#precipitation');
 const humidity = document.querySelector('#humidity');
+const ctx = document.getElementById('myChart');
+const ctxContainer = document.getElementById('myChartContainer');
 
 let coordinates = [];
+let tempChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: [],
+      datasets:[{
+        label: "",
+        data: [],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
 
 /*****PRINCIPALE FUNCTION***
 ***************************/
@@ -21,6 +41,7 @@ async function fetchCoordinates(cityChoice) {
         coordinates.push(dataCoordinates[0].lon);
 
         fetchWeather(coordinates[0], coordinates[1]);
+        fetchHourlyWeather(coordinates[0], coordinates[1]);
 
     } catch (error) {
         console.error("Failed to catch data :", error);
@@ -53,10 +74,48 @@ async function fetchWeather(latitude, longitude) {
     }
 }
 
+async function fetchHourlyWeather(latitude, longitude) {
+    try {
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,precipitation&past_days=2&forecast_days=2`);
+        const dataHourlyWeather = await response.json();
+
+        let AlldataHourlyWeather = dataHourlyWeather.hourly;
+        let tempHourly = AlldataHourlyWeather.temperature_2m;
+        let timeHourly = AlldataHourlyWeather.time;
+        ctxContainer.style.visibility = "visible";
+
+        tempChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+              labels: timeHourly,
+              datasets: [{
+                label: '°C',
+                data: tempHourly,
+                borderWidth: 1
+              }]
+            },
+            options: {
+              scales: {
+                y: {
+                  beginAtZero: true
+                }
+              }
+            }
+          });
+
+    } catch (error) {
+        console.error("Failed to catch data :", error);
+    }
+}
+
+
 
 /****START*****
 **************/
+ctxContainer.style.visibility = "hidden";
+
 buttonValidate.addEventListener('click', () => {
+    tempChart.destroy();
     coordinates = [];
     const cityInput = document.querySelector('#cityInput').value;
     fetchCoordinates(cityInput);
